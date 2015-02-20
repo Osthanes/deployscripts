c9719ad0711018cf691227d935d2c50fe3b1dfb8
Robbie Minshall <rjminsha@us.ibm.com>
2015-02-20 13:08:59 -0500

    updating container deploy

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -14,59 +14,44 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #********************************************************************************
-usage () { 
-    echo -e "${label_color}Usage:${no_color}"
-    echo "Set the following as a parameter on the job, or as an environment variable on the stage"
-    echo "DEPLOY_TYPE: "
-    echo "              simple: simply deploy a container and set the inventory"
-    echo "              red_black: deploy new container, assign floating IP address, keep original container"
-    echo ""
-    
-    echo "The following environement variables can be set on the stage:"
-    echo "DEPLOY_TYPE"
-    echo "API_KEY"
-    echo "IMAGE_NAME"
-    echo "CONTAINER_NAME"
-}
- 
+
 dump_info () {
     echo -e "${label_color}Container Information: ${no_color}"
-    echo "Running Containers: "
-    ice ps 
-    echo "Available floating IP addresses"
-    ice ip list --all
-    echo "All floating IP addresses"
-    ice ip list --all
+    echo -e "${label_color}Information about this organization and space${no_color}:"
+    echo "Summary:"
+    ice info 
+
  
-    if [[ (-z $IP_LIMIT) || (-z $CONTAINER_LIMIT) ]]; then 
-        echo "Expected Container Service Limits to be set on the environment"
-        return 1
-    fi 
- 
-    echo -e "${label_color}Current limitations:${no_color}"
-    echo "     # of containers: ${CONTAINER_LIMIT}"
-    echo "     # of floating IP addresses: ${IP_LIMIT}"
- 
-    WARNING_LEVEL="$(echo "$CONTAINER_LIMIT - 2" | bc)"
-    CONTAINER_COUNT=$(ice ps -q | wc -l | sed 's/^ *//') 
+    export CONTAINER_LIMIT=$(ice info | grep "Containers limit" | awk '{print $4}')
+    export IP_LIMIT=$(ice info | grep "Floating IPs limit" | awk '{print $5}')
+    export CONTAINER_COUNT=$(ice info | grep "Containers usage" | awk '{print $4}')
+    export IP_COUNT=$(ice info | grep "Floating IPs usage" | awk '{print $5}')
+    local WARNING_LEVEL="$(echo "$CONTAINER_LIMIT - 2" | bc)"
+
     if [ ${CONTAINER_COUNT} -ge ${CONTAINER_LIMIT} ]; then 
         echo -e "${red}You have ${CONTAINER_COUNT} containers running, and may reached the default limit on the number of containers ${no_color}"
-    elif [ $CONTAINER_COUNT -ge $WARNING_LEVEL ]; then
+    elif [ ${CONTAINER_COUNT} -ge ${WARNING_LEVEL} ]; then
         echo -e "${label_color}There are ${CONTAINER_COUNT} containers running, which is approaching the limit of ${CONTAINER_LIMIT}${no_color}"
     fi 
  
-    IP_COUNT_REQUESTED=$(ice ip list --all | grep "Number" | sed 's/.*: \([0-9]*\).*/\1/')
-    IP_COUNT_AVAILABLE=$(ice ip list | grep "Number" | sed 's/.*: \([0-9]*\).*/\1/')
-    echo "Number of IP Addresses currently requested: $IP_COUNT_REQUESTED"
-    echo "Number of requested IP Addresses that are still available: $IP_COUNT_AVAILABLE"
-    AVAILABLE="$(echo "$IP_LIMIT - $IP_COUNT_REQUESTED + $IP_COUNT_AVAILABLE" | bc)"
- 
-    if [ ${AVAILABLE} -eq 0 ]; then 
+    local AVAILABLE="$(echo "$IP_LIMIT - $IP_COUNT" | bc)"
+    if [ ${AVAILABLE} -le 0 ]; then 
         echo -e "${red}You have reached the default limit for the number of available public IP addresses${no_color}"
     else
         echo -e "${label_color}You have ${AVAILABLE} public IP addresses remaining${no_color}"
     fi  
+
+    echo "Groups: "
+    ice group list
+    echo "Routes: "
+    cf routes 
+    echo "Running Containers: "
+    ice ps 
+    echo "All floating IP addresses"
+    ice ip list --all
+    return 0
 }
+
 update_inventory(){
     local TYPE=$1
     local NAME=$2 
@@ -77,7 +62,7 @@ update_inventory(){
     fi  
     local ID="undefined"
     # find the container or group id 
-    if [ "$TYPE" == "container" ]; then 
+    if [ "$TYPE" == "ibm_containers" ]; then 
         ID=$(ice inspect ${NAME} | grep "\"Id\":" | awk '{print $2}')
         RESULT=$?
         if [ $RESULT -ne 0 ]; then
@@ -85,7 +70,8 @@ update_inventory(){
             ice ps 
             return 1 
         fi 
-    elif [ "${TYPE}" == "group"]; then
+
+    elif [ "${TYPE}" == "ibm_containers_group" ]; then
         ID=$(ice group inspect ${NAME} | grep "\"Id\":" | awk '{print $2}')
         if [ $RESULT -ne 0 ]; then
             echo -e "${red}Could not find group called $NAME${no_color}"
@@ -96,13 +82,15 @@ update_inventory(){
         echo -e "${red}Could not update inventory with unknown type: ${TYPE}${no_color}"
         return 1
     fi 
+
+    local JOB_TYPE=""
     # trim off junk 
     local temp="${ID%\",}"
     ID="${temp#\"}"
     echo "The ID of the $TYPE is: $ID"
  
     # find other inventory information 
-    echo -e "${label_color}Updating inventory with deployment $NAME of a $TYPE ${no_color}"
+    echo -e "${label_color}Updating inventory with $TYPE of $NAME ${no_color}"
     IDS_INV_URL="${IDS_URL%/}"
     IDS_REQUEST=$TASK_ID
     IDS_DEPLOYER=${JOB_NAME##*/}
@@ -120,17 +108,23 @@ update_inventory(){
             IDS_VERSION=$RTCBuildResultUUID
         fi
     fi
+
+    if [ -z "$IDS_RESOURCE" ]; then 
+        local IDS_RESOURCE="https://hub.jazz.net/pipeline"
+    fi 
+
+    if [ -z "$IDS_VERSION" ]; then 
+        local IDS_RESOURCE="1"
+    fi 
+
     IDS_RESOURCE=$CF_SPACE_ID
     if [ -z "$IDS_RESOURCE" ]; then 
         echo -e "${red}Could not find CF SPACE in environment, using production space id${no_color}"
-        IDS_RESOURCE="741f0392-92bd-45e2-9504-fcccfe20acd7"
-    else
-        echo "spaceID is ${IDS_RESOURCE}"
+    else 
+        # call IBM DevOps Service Inventory CLI to update the entry for this deployment
+        echo "bash ids-inv -a ${ACTION} -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ${TYPE} -u $IDS_INV_URL -v $IDS_VERSION"
+        bash ids-inv -a ${ACTION} -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ${TYPE} -u $IDS_INV_URL -v $IDS_VERSION
     fi 
- 
-    # call IBM DevOps Service Inventory CLI to update the entry for this deployment
-    echo "bash ids-inv -a insert -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ibm_containers -u $IDS_INV_URL -v $IDS_VERSION"
-    bash ids-inv -a $ACTION -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ibm_containers -u $IDS_INV_URL -v $IDS_VERSION
 }
  
 insert_inventory(){
@@ -152,8 +146,12 @@ wait_for (){
     STATE="unknown"
     while [[ ( $COUNTER -lt 60 ) && ("${STATE}" != "Running") ]]; do
         let COUNTER=COUNTER+1 
-        STATE=$(ice inspect $WAITING_FOR | grep "Status" | awk '{print $2}' | sed 's/"//g') && echo "${WAITING_FOR} is ${STATE}"
-        sleep 1
+        STATE=$(ice inspect $WAITING_FOR | grep "Status" | awk '{print $2}' | sed 's/"//g')
+        if [ -z "${STATE}" ]; then 
+            STATE="being placed"
+        fi 
+        echo "${WAITING_FOR} is ${STATE}"
+        sleep 2
     done
     if [ "$STATE" != "Running" ]; then
         echo -e "${red}Failed to start instance ${no_color}"
@@ -191,7 +189,7 @@ deploy_container() {
     wait_for ${MY_CONTAINER_NAME}
     RESULT=$?
     if [ $RESULT -eq 0 ]; then 
-        insert_inventory "container" ${MY_CONTAINER_NAME}
+        insert_inventory "ibm_containers" ${MY_CONTAINER_NAME}
     fi 
     return ${RESULT}
 }
@@ -215,35 +213,44 @@ deploy_red_black () {
     if [ $RESULT -ne 0 ]; then
         exit $RESULT
     fi
- 
-    COUNTER=${BUILD_NUMBER}
-    let COUNTER-=1
-    FOUND=0
+
+    echo "Cleaning up previous deployments.  Will keep ${CONCURRENT_VERSIONS} versions active."
+
+    if [ -z "$REMOVE_FROM" ]; then 
+        COUNTER=${BUILD_NUMBER}
+    else 
+        COUNTER=$REMOVE_FROM
+    fi 
+    local FOUND=0
     until [  $COUNTER -lt 1 ]; do
         ice inspect ${CONTAINER_NAME}_${COUNTER} > inspect.log 
         RESULT=$?
         if [ $RESULT -eq 0 ]; then
             echo "Found previous container ${CONTAINER_NAME}_${COUNTER}"
             # does it have a public IP address 
-            FLOATING_IP=$(cat inspect.log | grep "PublicIpAddress" | awk '{print $2}')
-            temp="${FLOATING_IP%\"}"
-            FLOATING_IP="${temp#\"}"
-            if [ $FOUND -eq 0 ]; then 
+            let FOUND+=1
+            if [ $FOUND -le $CONCURRENT_VERSIONS ]; then
+
+            elif [ $FOUND -le $CONCURRENT_VERSIONS ]; then
                 # this is the first previous deployment I have found
                 if [ -z "${FLOATING_IP}" ]; then 
-                    echo "${CONTAINER_NAME}_${COUNTER} did not have a floating IP so allocating one"
+                    FLOATING_IP=$(cat inspect.log | grep "PublicIpAddress" | awk '{print $2}')
+                    temp="${FLOATING_IP%\"}"
+                    FLOATING_IP="${temp#\"}"
+                fi 
+                if [ -z "${FLOATING_IP}" ]; then 
+                    echo "${CONTAINER_NAME}_${COUNTER} did not have a floating IP so will need to allocating one"
                 else 
                     echo "${CONTAINER_NAME}_${COUNTER} had a floating ip ${FLOATING_IP}"
                     ice ip unbind ${FLOATING_IP} ${CONTAINER_NAME}_${COUNTER}
+                    sleep 2
                     ice ip bind ${FLOATING_IP} ${CONTAINER_NAME}_${BUILD_NUMBER}
                     echo "keeping previous deployment: ${CONTAINER_NAME}_${COUNTER}"
                 fi 
-                FOUND=1
             else 
-                # remove
                 echo "removing previous deployment: ${CONTAINER_NAME}_${COUNTER}" 
                 ice rm ${CONTAINER_NAME}_${COUNTER}
-                delete_inventory "container" ${CONTAINER_NAME}_${COUNTER}
+                delete_inventory "ibm_containers" ${CONTAINER_NAME}_${COUNTER}
             fi  
         fi 
         let COUNTER-=1
@@ -270,7 +277,7 @@ deploy_red_black () {
             exit 1 
         fi 
         echo "Exporting TEST_URL:${TEST_URL}"
-        export TEST_URL="${URL_PROTOCOL}${FLOATING_IP}${URL_PORT}"
+        export TEST_URL="${URL_PROTOCOL}${FLOATING_IP}${PORT}"
     fi 
     echo -e "${green}Public IP address of ${CONTAINER_NAME}_${BUILD_NUMBER} is ${FLOATING_IP} and the TEST_URL is ${TEST_URL} ${no_color}"
 }
@@ -284,8 +291,11 @@ deploy_red_black () {
 if [ -z "$URL_PROTOCOL" ]; then 
  export URL_PROTOCOL="http://" 
 fi 
-if [ -z "$URL_PORT" ]; then 
- export URL_PORT=':80' 
+if [ -z "$PORT" ]; then 
+ export PORT=':80' 
+fi 
+if [ -z "$CONCURRENT_VERSIONS" ];then 
+    export CONCURRENT_VERSIONS=1
 fi 
  
  
@@ -294,12 +304,12 @@ if [ "${DEPLOY_TYPE}" == "simple" ]; then
     deploy_simple
 elif [ "${DEPLOY_TYPE}" == "simple_public" ]; then 
     deploy_public
-elif [ "${DEPLOY_TYPE}" == "red_black" ]; then 
+elif  [ "${DEPLOY_TYPE}" == "red_black" ]; then 
     deploy_red_black
 else 
     echo -e "${label_color}Defaulting to red_black deploy${no_color}"
     usage
     deploy_red_black
 fi 
-ice ps 
 dump_info
+exit 0

2c67b335ea871748e17c2bce28368919bd28d2f7
JACOB R. GARCOWSKI <jgarcows@us.ibm.com>
2015-05-14 14:43:17 -0400

    adding log_and_echo to deploygroup

--- deploygroup.sh
+++ deploygroup.sh
@@ -19,11 +19,11 @@
 source $(dirname "$0")/deploy_utilities.sh
 
 dump_info () {
-    echo -e "${label_color}Container Information: ${no_color}"
-    echo -e "${label_color}Information about this organization and space${no_color}:"
-    echo "Summary:"
+    log_and_echo "$LABEL" "Container Information: "
+    log_and_echo "$LABEL" "Information about this organization and space:"
+    log_and_echo "Summary:"
     local ICEINFO=$(ice info 2>/dev/null)
-    echo "$ICEINFO"
+    log_and_echo "$ICEINFO"
 
 
     export CONTAINER_LIMIT=$(echo "$ICEINFO" | grep "Containers limit" | awk '{print $4}')
@@ -34,9 +34,9 @@ dump_info () {
             local WARNING_LEVEL="$(echo "$CONTAINER_LIMIT - 2" | bc)"
 
             if [ ${CONTAINER_COUNT} -ge ${CONTAINER_LIMIT} ]; then
-                echo -e "${red}You have ${CONTAINER_COUNT} containers running, and may reached the default limit on the number of containers ${no_color}"
+                log_and_echo "$ERROR" "You have ${CONTAINER_COUNT} containers running, and may reached the default limit on the number of containers "
             elif [ ${CONTAINER_COUNT} -ge ${WARNING_LEVEL} ]; then
-                echo -e "${label_color}There are ${CONTAINER_COUNT} containers running, which is approaching the limit of ${CONTAINER_LIMIT}${no_color}"
+                log_and_echo "$WARN" "There are ${CONTAINER_COUNT} containers running, which is approaching the limit of ${CONTAINER_LIMIT}"
             fi
         fi
     fi
@@ -50,9 +50,9 @@ dump_info () {
             local MEM_WARNING_LEVEL="$(echo "$MEMORY_LIMIT - 512" | bc)"
 
             if [ ${MEMORY_USAGE} -ge ${MEMORY_LIMIT} ]; then
-                echo -e "${red}You are using ${MEMORY_USAGE} MB of memory, and may have reached the default limit for memory used ${no_color}"
+                log_and_echo "$ERROR" "You are using ${MEMORY_USAGE} MB of memory, and may have reached the default limit for memory used "
             elif [ ${MEMORY_USAGE} -ge ${MEM_WARNING_LEVEL} ]; then
-                echo -e "${label_color}You are using ${MEMORY_USAGE} MB of memory, which is approaching the limit of ${MEMORY_LIMIT}${no_color}"
+                log_and_echo "$WARN" "You are using ${MEMORY_USAGE} MB of memory, which is approaching the limit of ${MEMORY_LIMIT}"
             fi
         fi
     fi
@@ -67,16 +67,16 @@ dump_info () {
 #        echo -e "${label_color}You have ${AVAILABLE} public IP addresses remaining${no_color}"
 #    fi
 
-    echo "Groups: "
-    ice group list 2> /dev/null
-    echo "Routes: "
-    cf routes
-    echo "Running Containers: "
-    ice ps 2> /dev/null
-    echo "Floating IP addresses"
-    ice ip list 2> /dev/null
-    echo "Images:"
-    ice images
+    log_and_echo "Groups: "
+    log_and_echo `ice group list 2> /dev/null`
+    log_and_echo "Routes: "
+    log_and_echo `cf routes`
+    log_and_echo "Running Containers: "
+    log_and_echo `ice ps 2> /dev/null`
+    log_and_echo "Floating IP addresses"
+    log_and_echo `ice ip list 2> /dev/null`
+    log_and_echo "Images:"
+    log_and_echo `ice images`
 
     return 0
 }
@@ -86,7 +86,7 @@ update_inventory(){
     local NAME=$2
     local ACTION=$3
     if [ $# -ne 3 ]; then
-        echo -e "${red}updating inventory expects a three inputs: 1. type 2. name 3. action. Where type is either group or container, and the name is the name of the container being added to the inventory.${no_color}"
+        log_and_echo "$ERROR" "updating inventory expects a three inputs: 1. type 2. name 3. action. Where type is either group or container, and the name is the name of the container being added to the inventory."
         return 1
     fi
     local ID="undefined"
@@ -95,7 +95,7 @@ update_inventory(){
         ID=$(ice inspect ${NAME} | grep "\"Id\":" | awk '{print $2}')
         RESULT=$?
         if [ $RESULT -ne 0 ] || [ -z "${ID}" ]; then
-            echo -e "${red}Could not find container called $NAME${no_color}"
+            log_and_echo "$ERROR" "Could not find container called $NAME"
             ice ps
             return 1
         fi
@@ -103,12 +103,12 @@ update_inventory(){
     elif [ "${TYPE}" == "ibm_containers_group" ]; then
         ID=$(ice group inspect ${NAME} | grep "\"Id\":" | awk '{print $2}')
         if [ $RESULT -ne 0 ] || [ -z "${ID}" ]; then
-            echo -e "${red}Could not find group called $NAME${no_color}"
+            log_and_echo "$ERROR" "Could not find group called $NAME"
             ice group list
             return 1
         fi
     else
-        echo -e "${red}Could not update inventory with unknown type: ${TYPE}${no_color}"
+        log_and_echo "$ERROR" "Could not update inventory with unknown type: ${TYPE}"
         return 1
     fi
 
@@ -116,10 +116,10 @@ update_inventory(){
     # trim off junk
     local temp="${ID%\",}"
     ID="${temp#\"}"
-    echo "The ID of the $TYPE is: $ID"
+    log_and_echo "The ID of the $TYPE is: $ID"
 
     # find other inventory information
-    echo -e "${label_color}Updating inventory with $TYPE of $NAME ${no_color}"
+    log_and_echo "$LABEL" "Updating inventory with $TYPE of $NAME "
     IDS_INV_URL="${IDS_URL%/}"
     IDS_REQUEST=$TASK_ID
     IDS_DEPLOYER=${JOB_NAME##*/}
@@ -148,10 +148,10 @@ update_inventory(){
 
     IDS_RESOURCE=$CF_SPACE_ID
     if [ -z "$IDS_RESOURCE" ]; then
-        echo -e "${red}Could not find CF SPACE in environment, using production space id${no_color}"
+        log_and_echo "$ERROR" "Could not find CF SPACE in environment, using production space id"
     else
         # call IBM DevOps Service Inventory CLI to update the entry for this deployment
-        echo "bash ids-inv -a ${ACTION} -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ${TYPE} -u $IDS_INV_URL -v $IDS_VERSION"
+        log_and_echo "bash ids-inv -a ${ACTION} -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ${TYPE} -u $IDS_INV_URL -v $IDS_VERSION"
         bash ids-inv -a ${ACTION} -d $IDS_DEPLOYER -q $IDS_REQUEST -r $IDS_RESOURCE -s $ID -t ${TYPE} -u $IDS_INV_URL -v $IDS_VERSION
     fi
 }
@@ -168,7 +168,7 @@ delete_inventory(){
 wait_for_group (){
     local WAITING_FOR=$1
     if [ -z ${WAITING_FOR} ]; then
-        echo -e "${red}Expected container name to be passed into wait_for${no_color}"
+        log_and_echo "$ERROR" "Expected container name to be passed into wait_for"
         return 1
     fi
     COUNTER=0
@@ -180,14 +180,14 @@ wait_for_group (){
             STATE="being placed"
         fi
         if [ "${STATE}x" == "\"CREATE_FAILED\"x" ]; then
-            echo -e "${red}Failed to start group ${no_color}"
+            log_and_echo "$ERROR" "Failed to start group "
             return 1
         fi
-        echo "${WAITING_FOR} is ${STATE}"
+        log_and_echo "${WAITING_FOR} is ${STATE}"
         sleep 3
     done
     if [ "$STATE" != "\"CREATE_COMPLETE\"" ]; then
-        echo -e "${red}Failed to start group ${no_color}"
+        log_and_echo "$ERROR" "Failed to start group "
         return 1
     fi
     return 0
@@ -195,10 +195,10 @@ wait_for_group (){
 
 deploy_group() {
     local MY_GROUP_NAME=$1
-    echo "deploying group ${MY_GROUP_NAME}"
+    log_and_echo "deploying group ${MY_GROUP_NAME}"
 
     if [ -z MY_GROUP_NAME ];then
-        echo -e "${red}No container name was provided${no_color}"
+        log_and_echo "$ERROR" "No container name was provided"
         return 1
     fi
 
@@ -206,32 +206,32 @@ deploy_group() {
     ice group inspect ${MY_GROUP_NAME} > /dev/null
     local FOUND=$?
     if [ ${FOUND} -eq 0 ]; then
-        echo -e "${red}${MY_GROUP_NAME} already exists.${no_color}"
+        log_and_echo "$ERROR" "${MY_GROUP_NAME} already exists."
         exit 1
     fi
 
     local BIND_PARMS=""
     # validate the bind_to parameter if one was passed
     if [ ! -z "${BIND_TO}" ]; then
-        echo "Binding to ${BIND_TO}"
+        log_and_echo "Binding to ${BIND_TO}"
         local APP=$(cf env ${BIND_TO})
         local APP_FOUND=$?
         if [ $APP_FOUND -ne 0 ]; then
-            echo -e "${red}${BIND_TO} application not found in space.  Please confirm that you wish to bind the container to the application, and that the application exists${no_color}"
+            log_and_echo "$ERROR" "${BIND_TO} application not found in space.  Please confirm that you wish to bind the container to the application, and that the application exists"
         fi
         local VCAP_SERVICES=$(echo "${APP}" | grep "VCAP_SERVICES")
         local SERVICES_BOUND=$?
         if [ $SERVICES_BOUND -ne 0 ]; then
-            echo -e "${label_color}No services appear bound to ${BIND_TO}.  Please confirm that you have bound the intended services to the application.${no_color}"
+            log_and_echo "$WARN" "No services appear bound to ${BIND_TO}.  Please confirm that you have bound the intended services to the application."
         fi
         BIND_PARMS="--bind ${BIND_TO}"
     fi
     # create the group and check the results
-    echo "creating group: ice group create --name ${MY_GROUP_NAME} ${BIND_PARMS} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} --desired ${DESIRED_INSTANCES} ${AUTO} ${IMAGE_NAME}"
+    log_and_echo "creating group: ice group create --name ${MY_GROUP_NAME} ${BIND_PARMS} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} --desired ${DESIRED_INSTANCES} ${AUTO} ${IMAGE_NAME}"
     ice group create --name ${MY_GROUP_NAME} ${BIND_PARMS} ${PUBLISH_PORT} ${MEMORY} ${OPTIONAL_ARGS} --desired ${DESIRED_INSTANCES} ${AUTO} ${IMAGE_NAME}
     local RESULT=$?
     if [ $RESULT -ne 0 ]; then
-        echo -e "${red}Failed to deploy ${MY_GROUP_NAME} using ${IMAGE_NAME}${no_color}"
+        log_and_echo "$ERROR" "Failed to deploy ${MY_GROUP_NAME} using ${IMAGE_NAME}"
         return 1
     fi
 
@@ -244,14 +244,14 @@ deploy_group() {
             ice route map --hostname $ROUTE_HOSTNAME --domain $ROUTE_DOMAIN $MY_GROUP_NAME
             RESULT=$?
             if [ $RESULT -ne 0 ]; then
-                echo -e "${red}Failed to map $ROUTE_HOSTNAME $ROUTE_DOMAIN to $MY_GROUP_NAME.  Please ensure that the routes are setup correctly.  You can see this with cf routes when targetting the space for this stage.${no_color}"
+                log_and_echo "$ERROR" "Failed to map $ROUTE_HOSTNAME $ROUTE_DOMAIN to $MY_GROUP_NAME.  Please ensure that the routes are setup correctly.  You can see this with cf routes when targetting the space for this stage."
                 cf routes
             fi
         else
-            echo -e "${label_color}No route defined to be mapped to the container group.  If you wish to provide a Route please define ROUTE_HOSTNAME and ROUTE_DOMAIN on the Stage environment${no_color}"
+            log_and_echo "$WARN" "No route defined to be mapped to the container group.  If you wish to provide a Route please define ROUTE_HOSTNAME and ROUTE_DOMAIN on the Stage environment"
         fi
     else
-        echo -e "${red}Failed to deploy group${no_color}"
+        log_and_echo "$ERROR" "Failed to deploy group"
     fi
     return ${RESULT}
 }
@@ -261,13 +261,13 @@ deploy_simple () {
     deploy_group ${MY_GROUP_NAME}
     RESULT=$?
     if [ $RESULT -ne 0 ]; then
-        echo -e "${red}Error encountered with simple build strategy for ${CONTAINER_NAME}_${BUILD_NUMBER}${no_color}"
+        log_and_echo "$ERROR" "Error encountered with simple build strategy for ${CONTAINER_NAME}_${BUILD_NUMBER}"
         exit $RESULT
     fi
 }
 
 deploy_red_black () {
-    echo -e "${label_color}Example red_black container deploy ${no_color}"
+    log_and_echo "$LABEL" "Example red_black container deploy "
     # deploy new version of the application
     local MY_GROUP_NAME="${CONTAINER_NAME}_${BUILD_NUMBER}"
     deploy_group ${MY_GROUP_NAME}
@@ -283,13 +283,13 @@ deploy_red_black () {
             exit $RESULT
         fi
     else
-        echo "Not removing previous instances until after testing"
+        log_and_echo "Not removing previous instances until after testing"
     fi
     return 0
 }
 
 clean() {
-    echo "Cleaning up previous deployments.  Will keep ${CONCURRENT_VERSIONS} versions active."
+    log_and_echo "Cleaning up previous deployments.  Will keep ${CONCURRENT_VERSIONS} versions active."
 
     if [ -z "$REMOVE_FROM" ]; then
         COUNTER=${BUILD_NUMBER}
@@ -298,32 +298,32 @@ clean() {
     fi
     local FOUND=0
     until [  $COUNTER -lt 1 ]; do
-        echo "Looking for and inspecting ${CONTAINER_NAME}_${COUNTER}"
+        log_and_echo "Looking for and inspecting ${CONTAINER_NAME}_${COUNTER}"
         ice group inspect ${CONTAINER_NAME}_${COUNTER} > inspect.log
         local RESULT=$?
         if [ $RESULT -eq 0 ]; then
-            echo "Found previous container ${CONTAINER_NAME}_${COUNTER}"
+            log_and_echo "Found previous container ${CONTAINER_NAME}_${COUNTER}"
             let FOUND+=1
             if [ $FOUND -le $CONCURRENT_VERSIONS ]; then
                 # this is the previous version so keep it around
-                echo "keeping deployment: ${CONTAINER_NAME}_${COUNTER}"
+                log_and_echo "keeping deployment: ${CONTAINER_NAME}_${COUNTER}"
             elif [[ ( -n "${ROUTE_DOMAIN}" ) && ( -n "${ROUTE_HOSTNAME}" ) ]]; then
                 # remove this group
-                echo "removing route $ROUTE_HOSTNAME $ROUTE_DOMAIN from ${CONTAINER_NAME}_${COUNTER}"
+                log_and_echo "removing route $ROUTE_HOSTNAME $ROUTE_DOMAIN from ${CONTAINER_NAME}_${COUNTER}"
                 ice route unmap --hostname $ROUTE_HOSTNAME --domain $ROUTE_DOMAIN ${CONTAINER_NAME}_${COUNTER}
                 sleep 2
-                echo "removing group ${CONTAINER_NAME}_${COUNTER}"
+                log_and_echo "removing group ${CONTAINER_NAME}_${COUNTER}"
                 ice group rm ${CONTAINER_NAME}_${COUNTER}
                 delete_inventory "ibm_containers_group" ${CONTAINER_NAME}_${COUNTER}
             else
-                echo "removing group ${CONTAINER_NAME}_${COUNTER}"
+                log_and_echo "removing group ${CONTAINER_NAME}_${COUNTER}"
                 ice group rm ${CONTAINER_NAME}_${COUNTER}
                 delete_inventory "ibm_containers_group" ${CONTAINER_NAME}_${COUNTER}
             fi
         fi
         let COUNTER-=1
     done
-    echo "Cleaned up previous deployments"
+    log_and_echo "Cleaned up previous deployments"
     return 0
 }
 
@@ -333,13 +333,13 @@ clean() {
 # Check to see what deployment type:
 #   simple: simply deploy a container and set the inventory
 #   red_black: deploy new container, assign floating IP address, keep original container
-echo -e "${label_color}Deploying using ${DEPLOY_TYPE} strategy, for ${CONTAINER_NAME}, deploy number ${BUILD_NUMBER}${no_color}"
+log_and_echo "$LABEL" "Deploying using ${DEPLOY_TYPE} strategy, for ${CONTAINER_NAME}, deploy number ${BUILD_NUMBER}"
 
 check_num='^[0-9]+$'
 if [ -z "$DESIRED_INSTANCES" ]; then
     export DESIRED_INSTANCES=1
 elif ! [[ "$DESIRED_INSTANCES" =~ $check_num ]] ; then
-    echo -e "${label_color}DESIRED_INSTANCES value is not a number, defaulting to 1 and continue deploy process.${no_color}"
+    log_and_echo "$WARN" "DESIRED_INSTANCES value is not a number, defaulting to 1 and continue deploy process."
     export DESIRED_INSTANCES=1
 fi
 
@@ -351,11 +351,11 @@ else
 fi
 
 if [ -z "$ROUTE_HOSTNAME" ]; then
-    echo -e "${label_color}ROUTE_HOSTNAME not set.  Please set the desired or existing route hostname as an environment property on the stage.${no_color}"
+    log_and_echo "$WARN" "ROUTE_HOSTNAME not set.  Please set the desired or existing route hostname as an environment property on the stage."
 fi
 
 if [ -z "$ROUTE_DOMAIN" ]; then
-    echo -e "${label_color}ROUTE_DOMAIN not set, defaulting to mybluemix.net${no_color}"
+    log_and_echo "$WARN" "ROUTE_DOMAIN not set, defaulting to mybluemix.net"
     export ROUTE_DOMAIN="mybluemix.net"
 fi
 
@@ -364,17 +364,17 @@ if [ -z "$CONCURRENT_VERSIONS" ];then
 fi
 # Auto_recovery setting
 if [ -z "$AUTO_RECOVERY" ];then
-    echo -e "AUTO_RECOVERY not set, defaulting to false."
+    log_and_echo "AUTO_RECOVERY not set, defaulting to false."
     export AUTO=""
 elif [ "${AUTO_RECOVERY}" == "true" ] || [ "${AUTO_RECOVERY}" == "TRUE" ]; then
-    echo -e "${label_color}AUTO_RECOVERY set to true.${no_color}"
+    log_and_echo "$LABEL" "AUTO_RECOVERY set to true."
     export AUTO="--auto"
 elif [ "${AUTO_RECOVERY}" == "false" ] || [ "${AUTO_RECOVERY}" == "FALSE" ]; then
-    echo -e "${label_color}AUTO_RECOVERY set to false.${no_color}"
+    log_and_echo "$LABEL" "AUTO_RECOVERY set to false."
     export AUTO=""
 else
-    echo -e "${label_color}AUTO_RECOVERY value is invalid. Please enter false or true value.${no_color}"
-    echo -e "${label_color}Setting AUTO_RECOVERY value to false and continue deploy process.${no_color}"
+    log_and_echo "$WARN" "AUTO_RECOVERY value is invalid. Please enter false or true value."
+    log_and_echo "$LABEL" "Setting AUTO_RECOVERY value to false and continue deploy process."
     export AUTO=""
 fi
 
@@ -399,9 +399,9 @@ elif [ "${DEPLOY_TYPE}" == "clean" ]; then
 elif [ "${DEPLOY_TYPE}" == "red_black" ]; then
     deploy_red_black
 else
-    echo -e "${label_color}Currently only supporting 'red_black' deployment and 'clean' strategy${no_color}"
-    echo -e "${label_color}If you would like another strategy please fork https://github.com/Osthanes/deployscripts.git and submit a pull request${no_color}"
-    echo -e "${label_color}Defaulting to red_black deploy${no_color}"
+    log_and_echo "$WARN" "Currently only supporting 'red_black' deployment and 'clean' strategy"
+    log_and_echo "$WARN" "If you would like another strategy please fork https://github.com/Osthanes/deployscripts.git and submit a pull request"
+    log_and_echo "$WARN" "Defaulting to red_black deploy"
     deploy_red_black
 fi
 

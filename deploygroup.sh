b1427db50e7c2673b6d9fd22f43a1ef0b8f18e4b
eergun1 <eergun@us.ibm.com>
2015-07-29 11:12:16 -0400

    remove tabs

--- deploygroup.sh
+++ deploygroup.sh
@@ -224,10 +224,10 @@ deploy_group() {
         log_and_echo "$ERROR" "Failed to create group."
         sleep 3
 		
-		# display failure info
-		FAILED_GROUP=$(ice group inspect $MY_GROUP_NAME | grep "Failure" | cut -f2- -d':' | sed 's/,//g')
-		log_and_echo "The group ${MY_GROUP_NAME} failed due to:"
-		log_and_echo "$ERROR" "$FAILED_GROUP"
+        # display failure info
+        FAILED_GROUP=$(ice group inspect $MY_GROUP_NAME | grep "Failure" | cut -f2- -d':' | sed 's/,//g')
+        log_and_echo "The group ${MY_GROUP_NAME} failed due to:"
+        log_and_echo "$ERROR" "$FAILED_GROUP"
 		
         ice_retry group rm ${MY_GROUP_NAME}
         if [ $? -ne 0 ]; then

4ddf783ef897bb00656affba406cfb3fcf2bc47d
Bahram Sanaei <sanaei@us.ibm.com>
2016-02-29 15:16:23 -0500

    fixed the position for the output of the request IP

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -202,11 +202,7 @@ deploy_red_black () {
         if [ -z "${FLOATING_IP}" ];then
             log_and_echo "No any free IP address found. Requesting new IP"
             ice_retry_save_output ip request 2> /dev/null
-            if [ "$USE_ICE_CLI" = "1" ]; then
-                FLOATING_IP=$(awk '{print $4}' iceretry.log | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-            else
-                FLOATING_IP=$(awk '{print $3}' iceretry.log | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-            fi
+            FLOATING_IP=$(awk '{print $4}' iceretry.log | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
             RESULT=$?
             if [ $RESULT -ne 0 ]; then
                 cat iceretry.log
